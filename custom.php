2c4673eb83868d789224df482dcdc3871f62747d
Andrea Piccinelli <frasten@gmail.com>
2010-05-06 11:35:09 +0200

    Tradotto da Contact a Contatti.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- custom.php
+++ custom.php
@@ -26,7 +26,7 @@ function mystique_custom_nav_icons($nav_extra)
 	$nav_extra .= '<a href="http://www.facebook.com/TODO" class="nav-extra facebook" title="Seguici su Facebook"><span>Seguici su Facebook</span></a>';
 	//$nav_extra .= '<a href="http://gwfts.blogspot.com/" class="nav-extra blogger" title="Google Wave Blog"><span>Google Wave Blog</span></a>';
 	$nav_extra .= '<a href="http://www.youtube.com/TODO" class="nav-extra youtube" title="Canale YouTube"><span>Canale YouTube</span></a>';
-	$nav_extra .= '<a href="contatti" class="nav-extra email" title="Contact"><span>Contact</span></a>';
+	$nav_extra .= '<a href="contatti" class="nav-extra email" title="Contatti"><span>Contatti</span></a>';
 	//$nav_extra .= '<a href="http://www.google.com/profiles/frasten" class="nav-extra googlebuzz" title="Frasten @ Google Buzz"><span>Frasten @ Google Buzz</span></a>';
 	//$nav_extra .= '<a href="https://wave.google.com/wave/?pli=1#restored:wave:googlewave.com!w%252Bupyy5rPyA" class="nav-extra googlewave" title="Google Wave"><span>Google Wave</span></a>';
 
