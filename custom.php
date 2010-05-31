0730732bd2126e9e8490e011f162912d828b3273
Andrea Piccinelli <frasten@gmail.com>
2010-05-31 21:33:18 +0200

    Rimosso per ora il link a youtube. Fixes #65.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- custom.php
+++ custom.php
@@ -60,7 +60,7 @@ function mystique_custom_nav_icons($nav_extra)
 	//$nav_extra .= '<a href="http://uk.linkedin.com/in/pendlebury" class="nav-extra linkedin" title="LinkedIn"><span>LinkedIn</span></a>';
 	$nav_extra .= '<a href="http://www.facebook.com/apps/application.php?id=116144165091435" class="nav-extra facebook" title="Seguici su Facebook"><span>Seguici su Facebook</span></a>';
 	//$nav_extra .= '<a href="http://gwfts.blogspot.com/" class="nav-extra blogger" title="Google Wave Blog"><span>Google Wave Blog</span></a>';
-	$nav_extra .= '<a href="http://www.youtube.com/TODO" class="nav-extra youtube" title="Canale YouTube"><span>Canale YouTube</span></a>';
+	//$nav_extra .= '<a href="http://www.youtube.com/TODO" class="nav-extra youtube" title="Canale YouTube"><span>Canale YouTube</span></a>';
 	$nav_extra .= '<a href="'. get_bloginfo('url') .'/contatti" class="nav-extra email" title="Contatti"><span>Contatti</span></a>';
 	//$nav_extra .= '<a href="http://www.google.com/profiles/frasten" class="nav-extra googlebuzz" title="Frasten @ Google Buzz"><span>Frasten @ Google Buzz</span></a>';
 	//$nav_extra .= '<a href="https://wave.google.com/wave/?pli=1#restored:wave:googlewave.com!w%252Bupyy5rPyA" class="nav-extra googlewave" title="Google Wave"><span>Google Wave</span></a>';
